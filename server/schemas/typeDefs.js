ac8c2f0a15078642f1ba6fa43b027dfb944a5b93
Sarah Leder <Trinity16201@gmail.com>
2024-02-05 19:52:24 -0500

    added typedefs and resolvers. Tested routes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,39 @@
 const typeDefs = `
-  
+type Users {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+  }
+  type Artist {
+    _id: ID
+    name: String!
+    description: String!
+    city: String!
+    state: String!
+    images: [String]!
+    website: String!
+    email: String!
+    genre: [String]!
+  }
+  type Genre {
+    _id: ID
+    genre: String!
+    artists: [String]!
+  }
+  type Auth {
+    token: ID!
+    user: Users
+  }
+  type Query {
+    genres: [Genre]
+    me: Users
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
