fbed09d2e42dd9e128f164b7e0c31ea3cf20f3f7
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-06 19:08:21 -0500

    Push of the cleanDB component of the database.

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,2 +1,44 @@
 const db = require('../config/connection');
+const { Artists, Users, Genre } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const artistSeeds = require("./artistSeeds.json");
+const genreSeeds = require("./genreSeeds.json");
+const cleanDB = require("./cleanDB");
 
+db.once("open", async () => {
+  try {
+    await cleanDB("Artists", "artists");
+    await cleanDB("Genres", "genres");
+    await cleanDB("Users", "users");
+
+    await Artists.create(artistSeeds);
+    await Genre.create(genreSeeds);
+    await Users.create(userSeeds);
+
+    for (let i = 0; i < userSeeds.length; i++) {
+      const { _id, userAuthor } = await Users.create(userSeeds[i]);
+      const user = await Users.findOneAndUpdate(
+        { username: userAuthor },
+        {
+          $addToSet: {
+            users: _id,
+          },
+        }
+      );
+    }
+
+    for (let i = 0; i < artistSeeds.length; i++) {
+      await Artists.create(artistSeeds[i]);
+    }
+
+    for (let i = 0; i < genreSeeds.length; i++) {
+      await Genre.create(genreSeeds[i]);
+    }
+
+    console.log("all done!");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
