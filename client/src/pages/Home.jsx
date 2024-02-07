b6ea48b34a03a8a2a4c0e703422d97b9e54ab4f4
AgrastP <agrastp@yahoo.com>
2024-02-06 19:30:34 -0500

    added frontend pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+// import { useQuery } from '@apollo/client';
+
+// import ProfileList from '../components/ProfileList';
+
+// import { QUERY_PROFILES } from '../utils/queries';
+
+const Home = () => {
+//   const { loading, data } = useQuery(QUERY_PROFILES);
+//   const profiles = data?.profiles || [];
+
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <h2>Hello</h2>
+        {/* <div className="col-12 col-md-10 my-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ProfileList
+              profiles={profiles}
+              title="Here's the current roster of friends..."
+            />
+          )}
+        </div> */}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
