5ba5ad3fa83e254a4c51467cb44de0e87aa580bc
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-08 21:19:19 -0500

    Initial instance of header created with background color and logo.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,17 @@
-import { Link } from 'react-router-dom';
-
-import Auth from '../utils/auth';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
-  const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
-  };
   return (
-    <header>
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        
-        
-        <div>
-          {Auth.loggedIn() ? (
-            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-              Logout
-            </button>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
+    <div>
+    <header className="text-white py-1 fixed-top" style={{ height: '120px', backgroundColor: 'rgba(180, 180, 180, .10  )' }}>
+      <div className="container" style={{ opacity: 0.5 }}>
+      <div className="position-absolute" style={{ left: '100px', top: '20px' }}>
+        <img src="./assets/misc/logotemp.svg"  alt="logo" style={{ width: '200px', height: 'auto' }} /> 
       </div>
-    </header>
+      </div>      
+      </header>
+      </div>
   );
 };
 
