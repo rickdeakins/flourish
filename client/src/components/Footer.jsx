8d5533f010d4719a2aeeb43cf30456bfaa855ec8
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-10 13:38:02 -0500

    Logo path updated to prevent future issues rendering on the page.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Footer = () => {
 
   return (
-    <div id="footer" className="py-4 fixed-bottom margin-right text-secondary" style={{ height: '60px', backgroundColor: '#F2F2F2', marginTop: '10px'}}>
+    <div id="footer" className="py-4 fixed-bottom margin-right text-secondary" style={{ height: '60px', backgroundColor: '#F2F2F2'}}>
       <div className="container-fluid">
         <div className="row">
           <div className="col">
-            <p>© 2024 <img src="./assets/misc/logotemp.svg"  alt="logo" style={{ width: '70px', height: 'auto' }} /> | a visual artist showcase</p>
+            <p>© 2024 <img src="/assets/logotemp.svg"  alt="logo" style={{ width: '70px', height: 'auto' }} /> | a visual artist showcase</p>
           </div>
         </div>
       </div>
