5ac302f6788a908311b23b1885cbfd54f839cfbc
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-10 01:38:31 -0500

    Home page UI has been established. Additional text to be added - potentially svg.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,13 +17,15 @@ const Home = () => {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    minHeight: '100vh',
+    minHeight: '50vh',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%',
+    marginTop: '120px',
+    width: '70%',
     position: 'relative',
+    border: '4px solid'
   };
 
   const logout = () => {
@@ -32,25 +34,40 @@ const Home = () => {
   };
 
   return (
-    <Container fluid style={containerStyle}>
-      <Row className="h-100 align-items-center justify-content-center">
-        <Col md={12} className="text-center">
-          {Auth.loggedIn() ? ( // If user is logged in, show welcome message
-            <>
-              <button className="btn btn-lg btn-light m-2 fixed-bottom" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            // If user is not logged in, show login or signup form
-            <>
-              {activeTab === 'login' ? <Signup /> : <Login />}
-              <CustomButton activeTab={activeTab} handleTabChange={handleTabChange} />
-            </>
-          )}
+    <>
+      <Row>
+        <Col md={2} className="opacity-50" style={{ height: "auto", marginTop:'105px', marginBottom:'5px',border: '0.5px solid gray', backgroundColor: '#F2F2F2'}}>
+          <strong>Welcome to Flourish! </strong>
+          <p style={{ margin: "10px", textAlign: 'left-justify' }}><br/>
+            Flourish is a visual artist showcase where you can find a visual artist based on their genre of art.
+          </p>
         </Col>
+        <Col md={9}>
+          <Container fluid style={containerStyle}>
+            <Row className="justify-content-center opacity-75" style={{ marginTop: "20%" }}>
+              <Col md={12} className="text-center">
+                {Auth.loggedIn() ? (
+                  <>
+                    <button className="btn btn-lg btn-light m-2 fixed-bottom" onClick={logout}>
+                      Logout
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    {activeTab === 'login' ? <Signup /> : <Login />}
+                    <CustomButton activeTab={activeTab} handleTabChange={handleTabChange} />
+                  </>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+        {/* <Col md={1} className="h-100 align-items-left justify-content-center opacity-75" style={{ marginTop: "22%",border: '2px solid gray' }}>
+          <strong class="bg-color-dark">DISCOVER </strong>
+          <p>Your New Favorite Artist by Genre</p>
+        </Col> */}
       </Row>
-    </Container>
+    </>
   );
 };
 
