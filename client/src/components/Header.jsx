b4893e57000ae74687a09d9bf3a0d9490b9b8326
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-10 13:40:33 -0500

    Logo path updated to prevent future issues rendering on the page.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="text-white py-1 fixed-top" style={{ height: '120px', backgroundColor: '#F2F2F2' }}>
       <div className="container">
       <div className="position-absolute" style={{ left: '100px', top: '20px' }}>
-        <img src="./assets/misc/logotemp.svg"  alt="logo" style={{ width: '200px', height: 'auto' }}/>
+        <img src="/assets/logotemp.svg"  alt="logo" style={{ width: '200px', height: 'auto' }}/>
       </div>
       </div>      
       </header>
