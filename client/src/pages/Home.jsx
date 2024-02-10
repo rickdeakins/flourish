6b6608177084ddc4a680858d1bdbd063f8b037e9
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-10 13:50:17 -0500

    Updates to the structure of the page. Style attributes adjusted - pending isolation into variables.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,7 +36,8 @@ const Home = () => {
   return (
     <>
       <Row>
-        <Col md={2} className="opacity-50" style={{ height: "auto", marginTop:'105px', marginBottom:'5px',border: '0.5px solid gray', backgroundColor: '#F2F2F2'}}>
+        <Col md={2} className="opacity-50" 
+          style={{ height: "auto", marginTop:'105px', marginBottom:'5px', border: '0.5px solid gray', backgroundColor: '#F2F2F2'}}>
           <strong>Welcome to Flourish! </strong>
           <p style={{ margin: "10px", textAlign: 'left-justify' }}><br/>
             Flourish is a visual artist showcase where you can find a visual artist based on their genre of art.
@@ -63,8 +64,8 @@ const Home = () => {
           </Container>
         </Col>
         {/* <Col md={1} className="h-100 align-items-left justify-content-center opacity-75" style={{ marginTop: "22%",border: '2px solid gray' }}>
-          <strong class="bg-color-dark">DISCOVER </strong>
-          <p>Your New Favorite Artist by Genre</p>
+          <strong class="bg-color-dark">BE DISCOVERED! </strong>
+          <p>We welcome all visual artists to our platform.</p>
         </Col> */}
       </Row>
     </>
