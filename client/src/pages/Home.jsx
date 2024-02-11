7296e05845dda865bd86d79bfe68ca34a16aaa52
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-11 16:37:06 -0500

    About container styling updated for consistency across application. Login/signup initial instances swapped.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,43 +13,56 @@ const Home = () => {
     setActiveTab(tab);
   };
 
-  const containerStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    minHeight: '70vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '120px',
-    marginBottom: '120px',
-    width: '70%',
-    position: 'relative',
-    border: '4px solid',
-  };
-
   const logout = () => {
     // Implement your logout logic here
     Auth.logout();
   };
 
+//Style Attributes
+const containerStyle = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  minHeight: '70vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '120px',
+  marginBottom: '120px',
+  width: '70%',
+  position: 'relative',
+  border: '4px solid',
+};
+
+const aboutContainerStyle = {
+  height: 'auto',
+  marginTop: '105px',
+  marginBottom: '5px',
+  border: '0.5px solid gray',
+  borderRadius: '30px 30px 30px 30px',
+  textColor: '#333333',
+  boxShadow: '0 0 15px rgba(0, 0, 0, 0.5)',
+  textColor: '#333333',
+  backgroundColor: '#F2F2F2',
+  };
+
+  const horizontalLineStyle = {
+    border: '0',
+      borderTop: '2px solid black',
+      margin: '1px 0',
+  }
   return (
     <>
       <Row>
         <Col
-          md={2}
-          className="opacity-50"
-          style={{
-            height: 'auto',
-            marginTop: '105px',
-            marginBottom: '5px',
-            border: '0.5px solid gray',
-            backgroundColor: '#F2F2F2',
-          }}
+          md={3}
+          className="welcome"
+          style={aboutContainerStyle}
         >
-          <strong>Welcome to Flourish! </strong>
-          <p style={{ margin: '10px', textAlign: 'left-justify' }}>
+          <h3 style={{marginTop: '10px'}}>Welcome to Flourish! </h3>
+          <p style={{ margin: '10px', textAlign: 'left' }}>
+          <hr style={horizontalLineStyle} /> 
             <br />
             Flourish is a visual artist showcase where you can find a visual artist based on their genre of art.
           </p>
@@ -66,7 +79,7 @@ const Home = () => {
                   </>
                 ) : (
                   <>
-                    {activeTab === 'login' ? <Signup /> : <Login />}
+                    {activeTab === 'signup' ? <Login /> : <Signup />}
                     {Auth.loggedIn() && (
                       <button className="btn btn-lg btn-light m-2" style={{ marginBottom: '20px' }} onClick={logout}>
                         Logout
