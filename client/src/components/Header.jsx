acf1c73cff87d1e861f7fa76dabef3163007acef
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-12 17:03:29 -0500

    RESOLVED. Navlinks no longer overtaking header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,15 +33,15 @@ import NavTabs from './NavTabs';
 const Header = ({ isAuthenticated }) => {
   return (
     <div>
-      <header className="text-white py-1 fixed-top margin-right text-secondary" style={{ position: 'fixed', height: '120px', width: '100%', top: '0', left: '0', backgroundColor: '#F2F2F2', padding: '20px' }}>
+      <header className="text-white py-1 fixed-top margin-right text-secondary" style={{ position: 'fixed', height: '175px', width: '100%', top: '0', left: '0', backgroundColor: '#F2F2F2', padding: '20px' }}>
         <div className="container">
-          <div className="position-absolute" style={{ left: '80px', top: '15%' }}>
-            <img src="/assets/logotemp.svg" alt="logo" style={{ width: '200px', height: 'auto' }} />
+          <div className="position-absolute" style={{ left: '80px', top: '8%' }}>
+            <img src="/assets/logotemp.svg" alt="logo" style={{ width: '200px', height: 'auto', marginBottom: '10%' }} />
           </div>
-        <NavTabs isAuthenticated={isAuthenticated}/>
+        <NavTabs isAuthenticated={isAuthenticated} />
         </div>
       </header>
-      <div style={{ marginTop: '1%' }}>
+      <div style={{ marginTop: '6%' }}>
         {/* Content below the header */}
       </div>
     </div>
